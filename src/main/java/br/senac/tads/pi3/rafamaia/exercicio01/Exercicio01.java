41c7a00a533669b27dbfbd169d5caf18706ba51e
andre.rpinto <andre.rpinto@CAS419284C14726.senacsp.edu.br>
2015-08-14 22:07:20 -0300

    Segura essa pancada!!!

--- src/main/java/br/senac/tads/pi3/rafamaia/exercicio01/Exercicio01.java
+++ src/main/java/br/senac/tads/pi3/rafamaia/exercicio01/Exercicio01.java
@@ -15,31 +15,34 @@ public class Exercicio01 {
 
     static Scanner leitor = new Scanner(System.in);
 
-    static void entradaDeDados(String nome, String telefone, String data) {
+    static void entradaDeDados(String nome, String telefone, String data, String email) {
         System.out.println("*** Cadastro de Contatos ***");
-        
+
         System.out.print("Digite o nome do Contato: ");
         nome = leitor.nextLine();
-        
+
         System.out.print("Digite o telefone do Contato: ");
         telefone = leitor.nextLine();
-        
+
         System.out.print("Digite a data de nascimento do Contato: ");
         data = leitor.nextLine();
-        
+
+        System.out.print("Digite o Email do Contato: ");
+        email = leitor.nextLine();
+
         System.out.println("*** Contato salvo ***");
-        
+
+    }
+
+    public static void main(String[] args) {
+        String nome = null, telefone = null, data = null, email = null;
+
+        entradaDeDados(nome, telefone, data, email);
+
+    }
+
+    public static void saidaDeDados() {
         
     }
-    
-    
-    public static void main(String[] args) {
-        String nome = null, telefone = null, data = null;
-
-        entradaDeDados(nome, telefone, data);
-
-   }
-    
-    
 
 }
