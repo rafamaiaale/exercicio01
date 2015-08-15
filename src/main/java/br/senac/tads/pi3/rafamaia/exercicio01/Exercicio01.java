410753e9bae673b43ec3a14f578ac75980bff43a
rafael.malexandre <rafael.malexandre@CAS419287C14725.senacsp.edu.br>
2015-08-14 22:02:05 -0300

    Acabei de efetuar a função de entrada de dados.

--- src/main/java/br/senac/tads/pi3/rafamaia/exercicio01/Exercicio01.java
+++ src/main/java/br/senac/tads/pi3/rafamaia/exercicio01/Exercicio01.java
@@ -5,14 +5,41 @@
  */
 package br.senac.tads.pi3.rafamaia.exercicio01;
 
+import java.util.Scanner;
+
 /**
  *
  * @author rafael.malexandre
  */
 public class Exercicio01 {
 
-    public static void main(String[] args) {
-        System.out.println("HELLO WOOOOOOOOOOOOOOOOOOOOORLD!");
+    static Scanner leitor = new Scanner(System.in);
+
+    static void entradaDeDados(String nome, String telefone, String data) {
+        System.out.println("*** Cadastro de Contatos ***");
+        
+        System.out.print("Digite o nome do Contato: ");
+        nome = leitor.nextLine();
+        
+        System.out.print("Digite o telefone do Contato: ");
+        telefone = leitor.nextLine();
+        
+        System.out.print("Digite a data de nascimento do Contato: ");
+        data = leitor.nextLine();
+        
+        System.out.println("*** Contato salvo ***");
+        
+        
     }
+    
+    
+    public static void main(String[] args) {
+        String nome = null, telefone = null, data = null;
+
+        entradaDeDados(nome, telefone, data);
+
+   }
+    
+    
 
 }
