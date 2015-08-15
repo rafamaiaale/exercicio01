b693e4692e9f6da62407bc43bbfece9aada43417
rafael.malexandre <rafael.malexandre@CAS419284C14726.senacsp.edu.br>
2015-08-14 22:27:29 -0300

    Criação do banco de dados

--- src/main/java/br/senac/tads/pi3/rafamaia/exercicio01/Exercicio01.java
+++ src/main/java/br/senac/tads/pi3/rafamaia/exercicio01/Exercicio01.java
@@ -14,42 +14,43 @@ import java.util.Scanner;
 public class Exercicio01 {
 
     static Scanner leitor = new Scanner(System.in);
-    static Pessoa pessoa = new Pessoa();
-
-    static void entradaDeDados(String nome, String telefone, String data, String email) {
+    
+    static void entradaDeDados(Pessoa pessoa) {
 
         System.out.println("*** Cadastro de Contatos ***");
 
-        System.out.println("Digite o nome:");
+        System.out.println("Digite o nome: ");
         pessoa.setNome(leitor.nextLine());
         
-        System.out.println("Digite o e-mail:");
+        System.out.println("Digite o e-mail: ");
         pessoa.setEmail(leitor.nextLine());
         
-        System.out.println("Digite o telefone:");
+        System.out.println("Digite o telefone: ");
         pessoa.setTelefone(leitor.nextLine());
         
-        System.out.println("Digite o data de nascimento:");
+        System.out.println("Digite o data de nascimento: ");
         pessoa.setData(leitor.nextLine());
         
         
-        System.out.println("*** Contato salvo ***");
+        System.out.println("*** Contato salvo ***\n");
 
     }
 
     public static void main(String[] args) {
-        String nome = null, telefone = null, data = null, email = null;
-
-        entradaDeDados(nome, telefone, data, email);
+        Pessoa pessoa = new Pessoa();
+        
+        entradaDeDados(pessoa);
+        
+        saidaDeDados(pessoa);
 
     }
 
     public static void saidaDeDados(Pessoa pessoa) {
         System.out.println("***Dados do contato***");
-        System.out.println("Nome:" + pessoa.getNome());
-        System.out.println("Email:" + pessoa.getEmail());
-        System.out.println("Telefone:" + pessoa.getTelefone());
-        System.out.println("Data Nascimento:" + pessoa.getData());
+        System.out.println("Nome: " + pessoa.getNome());
+        System.out.println("Email: " + pessoa.getEmail());
+        System.out.println("Telefone: " + pessoa.getTelefone());
+        System.out.println("Data Nascimento: " + pessoa.getData());
     }
 
 }
