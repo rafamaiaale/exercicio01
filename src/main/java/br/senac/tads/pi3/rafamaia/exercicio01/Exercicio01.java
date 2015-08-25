420a11f807195e5662aae1c338f60c37226e346c
Andre <Andre@PC>
2015-08-25 17:17:01 -0300

    Insert e Listar

--- src/main/java/br/senac/tads/pi3/rafamaia/exercicio01/Exercicio01.java
+++ src/main/java/br/senac/tads/pi3/rafamaia/exercicio01/Exercicio01.java
@@ -8,9 +8,16 @@ package br.senac.tads.pi3.rafamaia.exercicio01;
 import java.beans.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -19,34 +26,77 @@ import java.util.Scanner;
 public class Exercicio01 {
 
     static Scanner leitor = new Scanner(System.in);
-    
+
     static void entradaDeDados(Pessoa pessoa) {
+
+        PreparedStatement stmt = null;
+        Connection conn = null;
+
+        Date dataNasc;
 
         System.out.println("*** Cadastro de Contatos ***");
 
         System.out.println("Digite o nome: ");
         pessoa.setNome(leitor.nextLine());
-        
+
         System.out.println("Digite o e-mail: ");
         pessoa.setEmail(leitor.nextLine());
-        
+
         System.out.println("Digite o telefone: ");
         pessoa.setTelefone(leitor.nextLine());
-        
-        System.out.println("Digite o data de nascimento: ");
-        pessoa.setData(leitor.nextLine());
-        
-        
-        System.out.println("*** Contato salvo ***\n");
+
+        System.out.println("Digite o data de nascimento no formato dd/mm/aaaa: ");
+        DateFormat formatadorData = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            dataNasc = formatadorData.parse(leitor.nextLine());
+        } catch (ParseException ex) {
+            Logger.getLogger(Exercicio01.class.getName()).log(Level.SEVERE, null, ex);
+            dataNasc = new Date();
+        }
+        pessoa.setData(dataNasc.toString());
+
+        String sql = "INSERT INTO TB_PESSOA (NM_PESSOA, DT_NASCIMENTO, VL_TELEFONE, VL_EMAIL) VALUES (?, ?, ?, ?)";
+        try {
+            conn = obterConexao();
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, pessoa.getNome());
+            stmt.setDate(2, new java.sql.Date(dataNasc.getTime()));
+            stmt.setString(3, pessoa.getTelefone());
+            stmt.setString(4, pessoa.getEmail());
+
+            stmt.executeUpdate();
+
+            System.out.println("*** Contato salvo ***\n");
+
+        } catch (SQLException ex) {
+            Logger.getLogger(Exercicio01.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Exercicio01.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(Exercicio01.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(Exercicio01.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
 
     }
 
     public static void main(String[] args) {
         Pessoa pessoa = new Pessoa();
-        
+
         entradaDeDados(pessoa);
-        
-        saidaDeDados(pessoa);
+
+        listarPessoas();
 
     }
 
@@ -57,28 +107,58 @@ public class Exercicio01 {
         System.out.println("Telefone: " + pessoa.getTelefone());
         System.out.println("Data Nascimento: " + pessoa.getData());
     }
-    public static void listarPessoas(){
-        Statement stmt = null;
+
+    public static void listarPessoas() {
+        java.sql.Statement stmt = null;
         Connection conn = null;
-        
-        String sql = "Select * from TB_PESSOA";
-    
-        try{            
+
+        String sql = "SELECT ID_PESSOA, NM_PESSOA, DT_NASCIMENTO, VL_TELEFONE, VL_EMAIL FROM TB_PESSOA";
+        try {
             conn = obterConexao();
             stmt = conn.createStatement();
-            
-            ResultSet resultados = stmt.execute(sql);
-        
+            ResultSet resultados = stmt.executeQuery(sql);
+
+            DateFormat formatadorData = new SimpleDateFormat("dd/MM/yyyy");
+
+            while (resultados.next()) {
+                Long id = resultados.getLong("ID_PESSOA");
+                String nome = resultados.getString("NM_PESSOA");
+                Date dataNasc = resultados.getDate("DT_NASCIMENTO");
+                String email = resultados.getString("VL_EMAIL");
+                String telefone = resultados.getString("VL_TELEFONE");
+                System.out.println(String.valueOf(id) + ", " + nome + ", " + formatadorData.format(dataNasc) + ", " + email + ", " + telefone);
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(Exercicio01.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Exercicio01.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(Exercicio01.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(Exercicio01.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
         }
-        
-        
+
     }
-    public Connection obterConexao() throws SQLException,ClassNotFoundException{
+
+    public static Connection obterConexao() throws SQLException, ClassNotFoundException {
         Connection conn = null;
         Class.forName("org.apache.derby.jdbc.ClientDataSource");
-        
-        conn = DriverManager.getConnection("jdbc:derby://localhost:");
-        
+
+        conn = DriverManager.getConnection("jdbc:derby://localhost:1527/agendabd;SecurityMechanism=3",
+                "app",
+                "app");
         return conn;
     }
 
